da00ec85b3ff564f74778b7c4d4ceeaf5daf4099
xyq <xyq135@gmail.com>
2022-06-07 15:59:59 +0800

    组件-RecordCard-修改图片上传方式

--- components/RecordCard.vue
+++ components/RecordCard.vue
@@ -1,39 +1,45 @@
 <template>
 	<view class="record-crad-container">
-		<view class="drug-name item">
-			<text class="label">药品名称：</text><input class="drug-name-input select" type="text" v-model="drugName"
-				placeholder="请输入药品名称" />
-		</view>
-		<view class="drug-avatar item">
-			<text class="label">药品图片：</text>
-			<view class="image-box" v-if="!tempImg" @click="addImg">
-				<uni-icons type="plusempty" size="50" color="#eee"></uni-icons>
+		<uni-card :is-shadow="true">
+			<view class="delete-record" @click="onHandleDeleteThisRecord">
+				<uni-icons type="trash" size="30" color="#ccc"></uni-icons>
 			</view>
-			<view class="image-box" v-if="tempImg">
-				<view class="close-icon" @click="delImage">
-					<uni-icons class="icon" type="closeempty" size="16" color="#fff"></uni-icons>
+			<view class="drug-name item">
+				<text class="label">药品名称：</text>
+				<input class="drug-name-input select" type="text" v-model="drugName" placeholder="请输入药品名称"></input>
+			</view>
+			<view class="drug-avatar item">
+				<text class="label">药品图片：</text>
+				<view class="image-box" v-if="!drugAvatar" @click="addImg">
+					<uni-icons type="plusempty" size="50" color="#eee"></uni-icons>
 				</view>
-				<image class="image" :src="tempImg"></image>
+				<view class="image-box" v-if="drugAvatar">
+					<view class="close-icon" @click="delImage">
+						<uni-icons class="icon" type="closeempty" size="16" color="#fff"></uni-icons>
+					</view>
+					<image class="image" :src="drugAvatar"></image>
+				</view>
 			</view>
-		</view>
-		<view class="drug-pear-count item">
-			<text class="label">每日剂量：</text>
-			<picker class="select" @change="onHandleDrugpearChange" mode=multiSelector :range="drugPearCountRange">
-				{{drugPearCount.count}}{{drugPearCount.unit}}
-			</picker>
-		</view>
-		<view class="drug-all-count item">
-			<text class="label">总药品量：</text>
-			<picker class="select" @change="onHandleDrugAllChange" mode=multiSelector :range="drugAllCountRange">
-				{{drugAllCount.count}}{{drugAllCount.unit}}{{drugAllCount.pearCount}}{{drugAllCount.pearUnit}}
-			</picker>
-		</view>
-		<view class="drug-day-eat-cout item">
-			<text class="label">日服药次数：</text>
-			<picker class="select" mode="selector" :range="drugDayEatRange" @change="onHandleDrugDayEatChange">
-				{{drugDayEatRange1[drugDayEatIndex-1]}}
-			</picker>
-		</view>
+			<view class="drug-pear-count item">
+				<text class="label">每日剂量：</text>
+				<picker class="select" @change="onHandleDrugpearChange" mode=multiSelector :range="drugPearCountRange">
+					{{drugPearCount.count}}{{drugPearCount.unit}}
+				</picker>
+			</view>
+			<view class="drug-all-count item">
+				<text class="label">总药品量：</text>
+				<picker class="select" @change="onHandleDrugAllChange" mode=multiSelector :range="drugAllCountRange">
+					{{drugAllCount.count}}{{drugAllCount.unit}}{{drugAllCount.pearCount}}{{drugAllCount.pearUnit}}
+				</picker>
+			</view>
+			<view class="drug-day-eat-cout item">
+				<text class="label">日服药次数：</text>
+				<picker class="select" mode="selector" :range="drugDayEatRange" @change="onHandleDrugDayEatChange">
+					{{drugDayEatRange1[drugDayEatIndex-1]}}
+				</picker>
+			</view>
+		</uni-card>
+
 	</view>
 
 </template>
@@ -41,18 +47,21 @@
 <script>
 	export default {
 		name: "RecordCard",
-		props:{
-			parentData:{}
+		props: {
+			parentData: {}
 		},
 		data() {
 			return {
-				drugName:this.parentData.drugName||'', //药品名称
-				drugAvatar: this.parentData.drugAvatar||'', //药品图片
+				pearId: this.parentData.pearId || (new Date().getTime() + (Math.floor(Math.random() * 100000 + 10000))
+					.toString(16)),
+				a: '1',
+				drugName: this.parentData.drugName || '', //药品名称
+				drugAvatar: this.parentData.drugAvatar || '', //药品图片
 				drugPearCount: this.parentData.pearCount || {
 					count: '',
 					unit: ''
 				}, //药品剂量
-				drugDayEatCount:this.parentData.drugDayEatCount || '', //一日几次
+				drugDayEatCount: this.parentData.drugDayEatCount || '', //一日几次
 				drugAllCount: this.parentData.allCount || {
 					count: '',
 					unit: '',
@@ -64,7 +73,7 @@
 					["请选择单位", '粒', '袋', '剂', '片', '支', "瓶", "盒"]
 				],
 				drugPearCountIndex1: 2,
-				drugPearCountIndex2: 2,
+				drugPearCountIndex2: 1,
 				drugAllCountRange: [
 					["请选择总数", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
 					["请选择单位", "盒", '袋', "瓶"],
@@ -80,22 +89,29 @@
 				drugDayEatRange: ["请选择每日服用次数", 1, 2, 3],
 				drugDayEatIndex: 3,
 				drugDayEatRange1: ['早', "早/晚", "早/中/晚"],
-				tempImg:this.parentData.drugAvatar || '', //临时地址
+				// tempImg: this.parentData.drugAvatar || '', //临时地址
 			};
 		},
 		created() {
 			// 初始化每日剂量
-			this.drugPearCount.count = this.parentData.pearCount ? this.parentData.pearCount.count : this.drugPearCountRange[0][this.drugPearCountIndex1];
-			this.drugPearCount.unit = this.parentData.pearCount ? this.parentData.pearCount.unit :this.drugPearCountRange[1][this.drugPearCountIndex2];
+			this.drugPearCount.count = this.parentData.pearCount ? this.parentData.pearCount.count : this
+				.drugPearCountRange[0][this.drugPearCountIndex1];
+			this.drugPearCount.unit = this.parentData.pearCount ? this.parentData.pearCount.unit : this.drugPearCountRange[
+				1][this.drugPearCountIndex2];
 			// 初始化总数
-			this.drugAllCount.count = this.parentData.allCount ?this.parentData.allCount.count : this.drugAllCountRange[0][this.drugAllCountIndex1];
-			this.drugAllCount.unit = this.parentData.allCount ?this.parentData.allCount.unit : this.drugAllCountRange[1][this.drugAllCountIndex2];
-			this.drugAllCount.pearCount = this.parentData.allCount ?this.parentData.allCount.pearCount :  this.drugAllCountRange[2][this.drugAllCountIndex3];
-			this.drugAllCount.pearUnit = this.parentData.allCount ?this.parentData.allCount.pearUnit : this.drugAllCountRange[3][this.drugAllCountIndex4];
+			this.drugAllCount.count = this.parentData.allCount ? this.parentData.allCount.count : this.drugAllCountRange[0]
+				[this.drugAllCountIndex1];
+			this.drugAllCount.unit = this.parentData.allCount ? this.parentData.allCount.unit : this.drugAllCountRange[1][
+				this.drugAllCountIndex2
+			];
+			this.drugAllCount.pearCount = this.parentData.allCount ? this.parentData.allCount.pearCount : this
+				.drugAllCountRange[2][this.drugAllCountIndex3];
+			this.drugAllCount.pearUnit = this.parentData.allCount ? this.parentData.allCount.pearUnit : this
+				.drugAllCountRange[3][this.drugAllCountIndex4];
 			// 一天吃几次
 			this.drugDayEatCount = this.parentData.drugDayEatCount || this.drugDayEatRange[this.drugDayEatIndex];
 			this.drugDayEatIndex = this.drugDayEatCount;
-		},	
+		},
 		methods: {
 			// 修改药品剂量
 			onHandleDrugpearChange(e) {
@@ -138,26 +154,61 @@
 			// 添加图片
 			addImg() {
 				uni.chooseImage({
-					success: (res) => {
-						this.tempImg = res.tempFilePaths[0];
+					count: 1,
+					success: async (res) => {
+						this.drugAvatar = res.tempFiles[0];
+						// 上传图片
+						if (res.tempFilePaths.length > 0) {
+							let url = res.tempFiles[0].path;
+							const result = await uniCloud.uploadFile({
+								filePath: url,
+								cloudPath: (new Date().getTime() + (Math.floor(Math.random() * 100000 + 10000))
+					.toString(16)) + url.substr(url.lastIndexOf('.')),
+							});
+							this.drugAvatar = result.filePath;
+							console.log(result)
+							console.log(this.drugAvatar)
+						}
 					}
 				})
+
 			},
 			//删除图片
 			delImage() {
-				this.tempImg = "";
+				this.drugAvatar = "";
 			},
 			// 将数据传给父组件
 			getRecordCardData() {
 				return {
+					pearId: this.pearId, //唯一ID
 					drugName: this.drugName, //药品名称
 					drugAvatar: this.drugAvatar, //药品图片
 					drugPearCount: this.drugPearCount, //药品剂量
 					drugDayEatCount: this.drugDayEatCount, //一日几次
-					drugAllCount:this.drugAllCount, //药品总量
-					tempImage:this.tempImg,
+					drugAllCount: this.drugAllCount, //药品总量
+					// tempImage: this.tempImg,
 				}
 			},
+
+			// 删除当前记录
+			onHandleDeleteThisRecord(e) {
+				uni.showModal({
+					title: "提示",
+					content: "确定要删除该药品",
+					success: res => {
+						if (res.confirm) {
+							// 确认删除
+							this.$emit("deleteOneRecord", this.pearId)
+						} else if (res.cancel) {
+							// 取消
+							uni.showToast({
+								title: '取消删除',
+								icon: "none"
+							});
+						}
+					}
+				})
+			}
 		}
 	}
 </script>
